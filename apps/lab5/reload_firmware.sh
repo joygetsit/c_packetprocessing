f4ade18f50df7c669ecda951b577d4d218588c05
Joydeep Pal <joy.xmofg@gmail.com>
2023-09-18 17:24:12 +0530

    update ip header file and added reload script

---
+++ apps/lab5/reload_firmware.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+./init/wire.sh stop
+make clean
+make
+sleep 1
+./init/wire.sh start wire.fw
+
+echo 'nfp-rtsym _counters'
+echo 'nfp-rtsym i32._stats'
+echo 'nfp-rtsym i33._stats'
+echo 'nfp-rtsym _customData'
+echo "nfp -m port -e show channel stats | grep -E 'UniCastPkts|port.ch'"
+echo 'nfp-reg mecsr:i32.me0.TimestampLow. mecsr:i32.me0.TimestampHgh.'
+echo ''
+
