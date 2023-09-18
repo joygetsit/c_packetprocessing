b9bee0c5c082c14f256b12b3de10550dcc254993
Joydeep Pal <joy.xmofg@gmail.com>
2023-09-18 12:42:44 +0530

    remove Tests and update gitignore

--- apps/lab5/Tests/after_TSN.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nfp-reg mecsr:i32.me0.Mailbox0=0 mecsr:i32.me0.Mailbox1=8 mecsr:i32.me0.Mailbox2=8
-
-nfp-reg xpb:Nbi0IsldXpbMap.NbiTopXpbMap.TrafficManager.TMShaperReg.ShaperRate{{0..3},144}.Rate=0x3fff
-nfp-reg xpb:Nbi0IsldXpbMap.NbiTopXpbMap.TrafficManager.TMShaperReg.ShaperRate1.Rate=1
-nfp-reg xpb:Nbi0IsldXpbMap.NbiTopXpbMap.TrafficManager.TrafficManagerReg.TrafficManagerConfig.ShaperEnable=1
